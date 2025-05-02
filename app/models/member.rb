bde99dc9f04af222d52eccc8c5763135bf5f561a
Anwar Rezk <arezk@silicon-mind.com>
2025-05-02 19:19:29 +0300

    Annotate models #1

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id         :bigint           not null, primary key
+#  user_id    :bigint           not null
+#  tenant_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Member < ApplicationRecord
   belongs_to :user
   belongs_to :tenant
